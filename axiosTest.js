ed32af33c9b18e4da08f1c98cbf3b7e51b1f7850
tongdesign <jakkapong.sara@gmail.com>
2021-06-23 02:37:17 +0700

    g

--- axiosTest.js
+++ axiosTest.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const xxx = axios.get("http://150.95.26.149:3000/test")
+const xxx = axios.get("http://150.95.26.149:3000/home")
       .then((res) => res)
       .then((json) => console.log(json));
 console.log(xxx.data);
