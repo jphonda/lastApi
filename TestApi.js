90da100c608d74a0660a543e17c3a15657275a69
tongdesign <jakkapong.sara@gmail.com>
2021-06-23 02:07:59 +0700

    g

--- TestApi.js
+++ TestApi.js
@@ -8,6 +8,10 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
+app.get('/test', () => {
+    res.json({msg: "hello world"})
+})
+
 app.listen(3000, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
